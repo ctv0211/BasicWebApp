fe4ca82a067337e7bc75008463d4e41c2aa07c1a
ge36pur <cuong.tran@tum.de>
2022-07-13 15:21:11 +0200

    Merge remote-tracking branch 'origin/main'

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -15,7 +15,7 @@ public class QueryProcessor {
            return "MyTeam";
         } else {
             // TODO extend the programm here
-            return "test";
+            return "";
         }
     }
 }
